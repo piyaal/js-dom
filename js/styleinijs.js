6e49424d7ced6a1db16927863c5713c42616c76b
piyaal <itpial48@gmail.com>
2023-10-23 09:52:15 +0600

    document query added

---
+++ js/styleinijs.js
@@ -0,0 +1,11 @@
+const sections=document.querySelectorAll('section');
+for(const section of sections) {
+//console.log(section);
+section.style.border='2px solid blue';
+section.style.marginBottom='5px';
+section.style.borderRadius='10px';
+section.style.padding='10px';
+section.style.backgroundColor='lightgray';
+}
+const container=document.getElementById('place-cn');
+container.style.backgroundColor='blue';
